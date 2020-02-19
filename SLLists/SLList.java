a10b78cc91648c01bb2576ed8edd118c053c8743
Xiaocheng Fei <61fxc.job@gmail.com>
2020-02-19 22:06:53 +1100

    Continue learning

--- SLLists/SLList.java
+++ SLLists/SLList.java
@@ -1,9 +1,17 @@
 package SLLists;
 
-import ReferenceRecursionAndList.ClassTime.IntNode;
-
 public class SLList {
-    public IntNode first;
+    public class IntNode {
+
+        public int item;
+        public IntNode next;
+
+        public IntNode(int i, IntNode n) {
+            item = i;
+            next = n;
+        }
+    }
+    private IntNode first;
 
     public SLList(int x)
     {
@@ -12,11 +20,18 @@ public class SLList {
 
     public void addFirst(int x)
     {
-        first = new IntNode(x, null);
+        first = new IntNode(x, first);
     }
 
     public int getFirst()
     {
         return first.item;
+    }
+
+    public static void main(String[] args) {
+        SLList L = new SLList(15);
+        L.addFirst(10);
+        L.addFirst(5);
+        System.out.println(L.getFirst());
     }
 }
