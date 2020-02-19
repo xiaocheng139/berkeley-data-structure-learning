ce17f508d8b41229c3ce987574b83d9772ef307a
jackfei01 <48191365+jackfei01@users.noreply.github.com>
2020-02-19 16:17:12 +1100

    Continue learning constructing array

---
+++ SLLists/SLList.java
@@ -0,0 +1,12 @@
+package SLLists;
+
+import ReferenceRecursionAndList.ClassTime.IntNode;
+
+public class SLList {
+    public IntNode first;
+
+    public SLList(int x)
+    {
+        first = new IntNode(x, null);
+    }
+}
