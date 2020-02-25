5b2840e08145238a3a68cdbf860c4434d5fe4b8b
jackfei01 <48191365+jackfei01@users.noreply.github.com>
2020-02-25 16:45:01 +1100

    Continue learning List discussion

---
+++ Lists/Discussion/Shock.java
@@ -0,0 +1,34 @@
+package Lists.Discussion;
+
+public class Shock
+{
+    public static int bang;
+    public static Shock baby;
+
+    public Shock() {
+        this.bang = 100;
+    }
+
+    public Shock (int num) {
+        this.bang = num;
+        baby = starter();
+        this.bang += num;
+    }
+
+    public static Shock starter() {
+        Shock gear = new Shock();
+        return gear;
+    }
+
+    public static void shrink(Shock statik) {
+        statik.bang -= 1;
+    }
+
+    public static void main(String[] args) {
+        Shock gear = new Shock(200);
+        System.out.println(gear.bang); // 300
+        shrink(gear); // 299
+        shrink(starter()); // 99
+        System.out.println(gear.bang); // 99
+    }
+}
