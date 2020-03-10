cb99699de0a6f8a663a9180a7e42b73f2727d0fa
jackfei01 <48191365+jackfei01@users.noreply.github.com>
2020-03-10 12:12:27 +1100

    Discussion 3 continue

--- Lists/Discussion/SLList.java
+++ Lists/Discussion/SLList.java
@@ -17,21 +17,51 @@ public class SLList {
 
     public void insert(int x, int position)
     {
+        if (first == null || position == 0)
+        {
+            addFirst(x);
+        }
+
         IntNode currentNode = first;
         int currentPosition = 0;
+
+        while (currentPosition + 1 < position && currentNode.next != null)
+        {
+            currentNode = currentNode.next;
+            currentPosition += 1;
+        }
+        IntNode newNode = new IntNode(x, null);
+        newNode.next = currentNode.next;
+        currentNode.next = newNode;
+    }
+
+    /* Reverse the elements in this linked list */
+    public void reverse()
+    {
+        if (first == null || first.next == null)
+        {
+            return;
+        }
+
+        IntNode currentNode = first.next;
+        first.next = null;
+
         while (currentNode != null)
         {
-            if (currentPosition + 1 < position)
-            {
-                currentNode = currentNode.next;
-                currentPosition += 1;
-            }
-            else
-            {
-                IntNode newNode = new IntNode(x, null);
-                newNode.next = currentNode.next;
-                currentNode.next = newNode;
-            }
+            IntNode temp = currentNode.next;
+            currentNode.next = first;
+            first = currentNode;
+            currentNode = temp;
         }
+    }
+
+    public static void main(String[] args) {
+        SLList exa = new SLList();
+        exa.addFirst(1);
+        exa.addFirst(2);
+        exa.addFirst(5);
+        exa.insert(10, 3);
+        exa.reverse();
+        System.out.println(123);
     }
 }
